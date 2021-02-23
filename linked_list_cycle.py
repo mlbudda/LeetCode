0526d031124e20a31c74e8ad66dfbd25877715ca
Ignas Markauskas <ignasmark5@gmail.com>
2021-02-23 22:28:15 +0200

    added docstring & comments

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -1,5 +1,9 @@
 #%%
 # 141. Linked List Cycle
+# Concept Hashing
+# * Success
+# Runtime: 56 ms, faster than 41.58%
+# Memory Usage: 18.2 MB, less than 5.45%
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -9,11 +13,20 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        checked = set()
+        """ Determines if the linked list has 
+        a cycle in it. """
+        # Declare map to store node address
+        node_map = set()
+
+        # If head is none no cycle is present
         while head is not None:
-            if head in checked:
+
+            # Checking if address is already in map
+            if head in node_map:
                 return True
-            checked.add(head)
+            
+            # If address not present, insert
+            node_map.add(head)
             head = head.next
         return False
 
